250664b2e6d5da2e45fb64578cb1500523c55771
alejandro.pi87@gmail.com <alejandro.pi87@gmail.com>
2026-03-17 20:35:29 +0100

    feat: excercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+
+const Statistics = ({good, neutral, bad}) => {
+    return <>
+        <h1>statistics</h1>
+        <p>good: {good}</p>
+        <p>neutral: {neutral}</p>
+        <p>bad: {bad}</p>
+        <p>all: {good + neutral + bad}</p>
+        <p>average: {(good - bad) / (good + neutral + bad) || 0} </p>
+        <p>positive: {good / (good + neutral + bad) * 100 || 0} % </p>
+   </>
+}
+
 const App = () => {
   // guarda los clics de cada botón en su propio estado
   const [good, setGood] = useState(0)
@@ -12,13 +25,7 @@ const App = () => {
         <button style={{"backgroundColor":"blue", "color": "white"}}onClick={() => setGood(good + 1)}>good</button>
         <button style={{"backgroundColor":"gray", "color": "white"}} onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button style={{"backgroundColor":"red", "color": "white"}}  onClick={() => setBad(bad + 1)}>bad</button>
-        <h1>statistics</h1>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>all: {good + neutral + bad}</p>
-        <p>average: {(good - bad) / (good + neutral + bad) || 0} </p>
-        <p>positive: {good / (good + neutral + bad) * 100 || 0} % </p>
+        <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
