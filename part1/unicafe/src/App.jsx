959204fb090cfe88ea8360bd86378ce77081819f
alejandro.pi87@gmail.com <alejandro.pi87@gmail.com>
2026-03-17 20:37:45 +0100

    feat: excercise 1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,12 +4,18 @@ import { useState } from 'react'
 const Statistics = ({good, neutral, bad}) => {
     return <>
         <h1>statistics</h1>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-        <p>all: {good + neutral + bad}</p>
-        <p>average: {(good - bad) / (good + neutral + bad) || 0} </p>
-        <p>positive: {good / (good + neutral + bad) * 100 || 0} % </p>
+        {good + neutral + bad === 0 ? (
+            <p>No feedback given</p>
+        ) : (
+            <>
+                <p>good: {good}</p>
+                <p>neutral: {neutral}</p>
+                <p>bad: {bad}</p>
+                <p>all: {good + neutral + bad}</p>
+                <p>average: {(good - bad) / (good + neutral + bad) || 0} </p>
+                <p>positive: {good / (good + neutral + bad) * 100 || 0} % </p>
+            </>
+        )}      
    </>
 }
 
