e3297ef4b3ec5602a6e478f93273339c19261789
alejandro.pi87@gmail.com <alejandro.pi87@gmail.com>
2026-03-18 13:12:28 +0100

    feat: excercise 2.4 and 2.5

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -3,9 +3,7 @@ import { Part } from './Part';
 export const Content = ({parts}) => { 
   return (
     <>
-      <Part part={parts[0].name} exercises={parts[0].exercises}/>
-      <Part part={parts[1].name} exercises={parts[1].exercises}/>
-      <Part part={parts[2].name} exercises={parts[2].exercises}/>     
+      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}        
    </>
   )
 }
