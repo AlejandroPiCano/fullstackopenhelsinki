bdeffa83b9cb7698048062d4904ff90cc568a00a
alejandro.pi87@gmail.com <alejandro.pi87@gmail.com>
2026-03-18 13:03:49 +0100

    feat: excercise 2.3

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,5 +1,8 @@
 
 export const Total = ({parts}) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0);
+  const total = parts.reduce((sum, part) => { 
+    console.log("sum:",  sum, "exercises:", part.exercises);
+    return sum + part.exercises;
+  }, 0);
   return <p style={{"fontWeight": "Bold"}}>Total of {total} exercises </p>;
 };
